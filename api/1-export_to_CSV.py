96cd812578eee5966c4cceba50b7bdbd97523770
Korchea <6334@holbertonstudents.com>
2023-08-22 09:36:24 -0700

    Task 0 format solved

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,16 +7,13 @@ from sys import argv
 if __name__ == "__main__":
     req = "https://jsonplaceholder.typicode.com/users/{}"
     req_all_task = "https://jsonplaceholder.typicode.com/todos"
-    user = ""
     id = int(argv[1])
+    txt = '"{}","{}","{}","{}"\n'
     req_users = requests.get(req.format(id)).json()
     req_all = requests.get(req_all_task).json()
-    filename = "{}.csv".format(id)
-    with open(filename, 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ',
-                                quoting=csv.QUOTE_MINIMAL)
-        for all in req_all:
-            if all['userId'] == id:
-                work = all['completed']
-                work_name = all['title']
-                spamwriter.writerow([id, req_users['name'], work, work_name])
+    username_task = req_users['username']
+    with open("{}.csv".format(id), "w") as file:
+        for i in req_all:
+            if i['userId'] == id:
+                file.write(txt.format(id, username_task,
+                                      i['completed'], i['title']))
